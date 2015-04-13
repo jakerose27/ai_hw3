f5ddf0a2c4f5f74531accc80d77dd4c0ccf39e27
Halvi3 <christopher.halverson@gmail.com>
2015-04-13 16:34:49 -0400

    Non-permutation based offer generation

--- ceh4ws.py
+++ ceh4ws.py
@@ -22,6 +22,8 @@ class Negotiator(BaseNegotiator):
         self.init_enemy_util = 0
         self.enemy_greed_slope = 0
         self.THRESHOLD = 0.9
+        self.our_last_offer = []
+        self.enemy_first_offer = []
 
     def initialize(self, preferences, iter_limit):
         self.preferences = preferences[:]
@@ -30,11 +32,13 @@ class Negotiator(BaseNegotiator):
         print("MAX_UTIL: {util}".format(util=self.max_utility))
         self.possibilities = self.get_possibilities(preferences)
         self.threshold_increment = (0.9-0.5)/(iter_limit - 3)
+        self.offers_made = []
 
     def make_offer(self, offer):
         print("NEGOTIATION ROUND {round}".format(round=self.round))
         new_offer = ""
         if self.round == 1:
+            self.our_last_offer = self.preferences[:]
             self.first = True if (offer == None) else False
             new_offer = self.make_first_offer(offer)
         elif self.round == self.iter_limit:
@@ -42,16 +46,23 @@ class Negotiator(BaseNegotiator):
         elif self.round == self.iter_limit+1:
             new_offer = self.accept(offer)
         else:
+            if self.round == 2:
+                if self.first:
+                    self.enemy_first_offer = offer[:]
             if self.worth_it(offer):
                 self.offer = offer[:]
+                self.our_last_offer = offer[:]
                 return self.offer
             else:
-                bad_offer = self.possibilities.keys().index(tuple(self.offer))
-                new_offer = self.possibilities.keys()[bad_offer-1][:]
+                # bad_offer = self.possibilities.keys().index(tuple(self.offer))
+                # new_offer = self.possibilities.keys()[bad_offer-1][:]
+                new_offer = self.generate_offer()
 
         self.offer = new_offer[:]
         print("OUR_UTIL: {util}".format(util=self.get_utility(self.offer)))
         self.round += 1
+        self.our_last_offer = new_offer[:]
+        self.offers_made.append(new_offer)
         return new_offer[:]
 
     def receive_utility(self, utility):
@@ -70,6 +81,37 @@ class Negotiator(BaseNegotiator):
         print("LAST SLOPE: {util}".format(util=slope))
         self.enemy_avg_slope = slope
 
+    def generate_offer(self):
+        possibilities = self.make_possibilities(self.our_last_offer)
+        max = []
+        max_u = 0
+        for p in possibilities:
+            if p not in self.offers_made:
+                max = p if self.get_utility(p) > max_u else max
+                max_u = self.get_utility(max)
+        return max
+
+
+    def make_possibilities(self, the_list):
+        print("Possibilities of {the_list}".format(the_list=the_list))
+        poss_list = []
+        for item in the_list:
+            copy1 = the_list[:]
+            copy2 = the_list[:]
+            index = the_list.index(item)
+            if index != 0:
+                removed = copy1.pop(index)
+                copy1.insert(index-1, removed)
+                poss_list.append(copy1)
+            if index != len(the_list)-1:
+                removed = copy2.pop(index)
+                copy2.insert(index+1, removed)
+                poss_list.append(copy2)
+        return poss_list
+
+
+
+
     # A round has ended. Store the results.
     def receive_results(self, results):
         self.results[self.round] = results
@@ -78,6 +120,7 @@ class Negotiator(BaseNegotiator):
         if self.first:
             return self.preferences
         else:
+            self.enemy_first_offer = offer[:]
             return self.preferences
 
     def make_last_offer(self, offer):
--- negotiator_framework.py
+++ negotiator_framework.py
@@ -68,7 +68,7 @@ if __name__ == "__main__":
     score_a = score_b = 0
     # We will replace Negotiator here with <your id>_Negotiator, as specified in the Readme
     negotiator_a = ceh4ws()
-    negotiator_b = pref()
+    negotiator_b = ceh4ws()
     # negotiator_b = ceh4ws()
     for scenario in argv[1:]:
         # Get the scenario parameters
